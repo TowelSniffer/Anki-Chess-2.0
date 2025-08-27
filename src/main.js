fa902a1c5853b66a31abc85636cf9c355ff340d1
TowelSniffer <52064198+TowelSniffer@users.noreply.github.com>
2025-08-27 12:40:28 +1000

    release

--- src/main.js
+++ src/main.js
@@ -19,20 +19,11 @@ function getUrlParam(name, defaultValue) {
 
 // --- Configuration ---
 const config = {
-    pgn: getUrlParam("PGN", `[Event "?"]
-[Site "?"]
-[Date "2023.02.13"]
-[Round "?"]
-[White "White"]
-[Black "Black"]
-[Result "*"]
-[FEN "8/5pkp/1pB3p1/1Pn5/pbR5/5P2/3r2P1/7K w - - 0 45"]
-[SetUp "1"]
-
-45. Rxb4 a3 {EV: 99.8%, N: 43.09% of 91.3k} (45... Rd1+ {EV: 99.6%, N: 6.39% of
-91.3k} 46. Kh2 a3) 46. Rb1 {EV: 0.2%, N: 12.07% of 138k} a2 {EV: 99.8%, N:
-29.82% of 31.0k} 47. Ra1 {EV: 0.3%, N: 14.64% of 18.2k} Nb3 {EV: 99.6%, N:
-40.53% of 24.4k} *`),
+    pgn: getUrlParam("PGN", `[Event "Puzzle"]
+[Site "https://www.lichess.org/training/qXllP"]
+[Themes "opening fork long crushing"]
+[FEN "rnbqk2r/pp3ppp/2pb1n2/3p4/3P4/1P2PN2/P4PPP/RNBQKB1R w KQkq - 1 7"]
+[SetUp "1"] { Puzzle qXllP with themes: opening fork long crushing } 7. Ba3 Bxa3 8. Nxa3 Qa5+ 9. Nd2 Qxa3 *`),
     fontSize: getUrlParam("fontSize", 16),
     ankiText: getUrlParam("userText", null),
     frontText: getUrlParam("frontText", 'false') === 'true',
@@ -224,6 +215,7 @@ function highlightCurrentMove(pgnPath) {
 }
 
 function drawArrows(cg, chess, redraw) {
+    state.chessGroundShapes = state.chessGroundShapes.filter(shape => shape.brush !== 'stockfinished' && shape.brush !== 'stockfish');
     if (redraw) {
         cg.set({ drawable: { shapes: state.chessGroundShapes } });
         return;
@@ -236,9 +228,7 @@ function drawArrows(cg, chess, redraw) {
     if (config.boardMode === 'Puzzle' && config.disableArrows) {
         return;
     }
-    if (!state.analysisToggledOn) {
-        state.chessGroundShapes = state.chessGroundShapes.filter(shape => shape.brush !== 'stockfinished' && shape.brush !== 'stockfish');
-    }
+
     let expectedMove = state.expectedMove;
     let expectedLine = state.expectedLine;
     let puzzleMove;
@@ -325,7 +315,6 @@ function updateBoard(cg, chess, move, quite, commentRewrite) { // animate user/a
             state.pgnPath = currentMove.pgnPath;
         }
     }
-
     if (move.san.includes("=") && state.promoteAnimate) {
         const tempChess = new Chess(chess.fen());
         tempChess.load(chess.fen());
@@ -341,8 +330,14 @@ function updateBoard(cg, chess, move, quite, commentRewrite) { // animate user/a
             });
             cg.set({ animation: { enabled: true} })
         }, 200)
+    } else if (move.flags.includes("e")) {
+        cg.set({ animation: { enabled: false} })
+        cg.set({
+            fen: chess.fen(),
+        });
+        cg.set({ animation: { enabled: true} })
     } else {
-        cg.set({ fen: chess.fen() });
+            cg.set({ fen: chess.fen() });
     }
     cg.set({
         check: chess.inCheck(),
@@ -442,13 +437,13 @@ function handleStockfishMessages(event) {
                     const tempChess = new Chess(state.analysisFen);
                     const moveObject = tempChess.move(firstMove);
                     if (moveObject && state.analysisToggledOn) {
-                        state.chessGroundShapes = state.chessGroundShapes.filter(shape => shape.brush !== 'stockfish' && shape.brush !== 'stockfinished');
+                        state.chessGroundShapes = state.chessGroundShapes.filter(shape => shape.brush !== 'stockfish' && shape.brush !== 'stockfinished' && shape.brush !== 'stockfishShadow');
                         state.chessGroundShapes.push({
-                            orig: moveObject.from,
-                            dest: moveObject.to,
-                            brush: 'stockfish',
-                            san: moveObject.san,
-                        });
+                                orig: moveObject.from,
+                                dest: moveObject.to,
+                                brush: 'stockfish',
+                                san: moveObject.san,
+                            });
                         cg.set({drawable: {shapes: state.chessGroundShapes}});
                     }
                 }
@@ -527,8 +522,8 @@ function toggleStockfishAnalysis() {
         if (state.isStockfishBusy) {
             stockfish.postMessage('stop'); // Tell the engine to stop thinking
         }
-        drawArrows(cg, chess);
     }
+    drawArrows(cg, chess);
 }
 
 function setNavButtonsDisabled(disabled) {
@@ -1001,10 +996,14 @@ function reload() {
         drawable: {
             enabled: true,
             brushes: {
-                stockfish: { key: 'stockfish', color: '#FF0035', opacity: 1, lineWidth: 6 },
-                stockfinished: { key: 'stockfinished', color: '#FF0035', opacity: 1, lineWidth: 7 },
+                stockfish: { key: 'stockfish', color: '#e5e5e5', opacity: 1, lineWidth: 7 },
+                stockfinished: { key: 'stockfinished', color: 'white', opacity: 1, lineWidth: 7 },
                 mainLine: { key: 'mainLine', color: '#66AA66', opacity: 1, lineWidth: 9 },
                 altLine: { key: 'altLine', color: '#66AAAA', opacity: 1, lineWidth: 9 },
+                green: { opacity: 0.7, lineWidth: 9 },
+                red: { opacity: 0.7, lineWidth: 9 },
+                blue: { opacity: 0.7, lineWidth: 9 },
+                yellow: { opacity: 0.7, lineWidth: 9 },
             },
         },
     });
@@ -1119,10 +1118,20 @@ function reload() {
     function navForward() {
         if (config.boardMode === 'Puzzle' || !state.pgnState || !state.expectedMove?.notation) return;
         const tempChess = new Chess(chess.fen());
-        const move = tempChess.move(state.expectedMove?.notation?.notation)
-        if (move) {
+        const move = tempChess.move(state.expectedMove?.notation?.notation);
+        if (move.flags.includes("e")) {
+            chess.move(move.san)
+            cg.set({
+                fen: chess.fen(),
+            });
+            state.count++;
+            state.expectedMove = state.expectedLine[state.count];
+            drawArrows(cg, chess)
+        } else if (move) {
             puzzlePlay(cg, chess, null, move.from, move.to);
         }
+        document.querySelector("#navBackward").disabled = false;
+        document.querySelector("#resetBoard").disabled = false;
         if (!state.expectedMove || typeof state.expectedMove === 'string') {
             document.querySelector("#navForward").disabled = true;
         }
@@ -1207,6 +1216,16 @@ function reload() {
         } else if (event.deltaY > 0) {
             // Perform actions for scrolling down
             navForward()
+        }
+    });
+    document.addEventListener('keydown', (event) => { // scroll Navigation
+        event.preventDefault();
+        if (event.key === 'ArrowLeft') {
+            navBackward()
+        } else if (event.key === 'ArrowRight') {
+            navForward()
+        } else if (event.key === 'ArrowDown') {
+            resetBoard()
         }
     });
 }
