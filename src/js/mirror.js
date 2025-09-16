2d77c13851f78b4720c64685f12f4a0149e1ae23
EnPriseWithEase <565490+EnPriseWithEase@users.noreply.github.com>
2025-09-16 00:10:35 -0400

    parse arrows from pgn comments and display in Viewer

--- src/js/mirror.js
+++ src/js/mirror.js
@@ -6,6 +6,15 @@ export function assignMirrorState(pgn) {
   return mirrorState;
 }
 
+export function mirrorGameCommentArrows(pgn, mirrorState) {
+  if (pgn.gameComment?.colorArrows?.length > 0) {
+     const notationMap = getMirrorNotationMap(mirrorState);
+     for (let i = 0; i < pgn.gameComment.colorArrows.length; i++) { 
+        pgn.gameComment.colorArrows[i] = pgn.gameComment.colorArrows[i].split('').map(char => notationMap[char] || char).join('');
+     }
+  }
+}
+
 export function mirrorFenRow(row) {
   let result = row.split('').reverse().join('');
   return result;
@@ -43,22 +52,30 @@ export function swapCase(str) {
   ).join('');
 }
 
-export function mirrorMove(move, mirrorState) {
-  var notations = {}
-
+export function getMirrorNotationMap(mirrorState) {
   const notationMaps = {
     "invert_mirror": { q: 'q', a: 'a', b: 'b', c: 'c', d: 'd', e: 'e', f: 'f', g: 'g', h: 'h', 1: '8', 2: '7', 3: '6', 4: '5', 5: '4', 6: '3', 7: '2', 8: '1' },
     "invert": { q: 'q', a: 'h', b: 'g', c: 'f', d: 'e', e: 'd', f: 'c', g: 'b', h: 'a', 1: '8', 2: '7', 3: '6', 4: '5', 5: '4', 6: '3', 7: '2', 8: '1' },
     "original_mirror": { q: 'q', a: 'h', b: 'g', c: 'f', d: 'e', e: 'd', f: 'c', g: 'b', h: 'a', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8' },
     "original": { q: 'q', a: 'a', b: 'b', c: 'c', d: 'd', e: 'e', f: 'f', g: 'g', h: 'h', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8' }
   }
+  return notationMaps[mirrorState] || notationMaps.original;
+}
 
-  const notationMap = notationMaps[mirrorState] || notationMaps.original;
+export function mirrorMove(move, mirrorState) {
+
+  const notationMap = getMirrorNotationMap(mirrorState);
 
   if (move.notation.disc) move.notation.disc = move.notation.disc.split('').map(char => notationMap[char] || char).join('');
   if (move.notation.col) move.notation.col = move.notation.col.split('').map(char => notationMap[char] || char).join('');
   if (move.notation.row) move.notation.row = move.notation.row.split('').map(char => notationMap[char] || char).join('');
   move.notation.notation = move.notation.notation.split('').map(char => notationMap[char] || char).join('');
+
+  if (move.commentDiag?.colorArrows?.length > 0) {
+    for (let i = 0; i < move.commentDiag.colorArrows.length; i++) {
+        move.commentDiag.colorArrows[i] = move.commentDiag.colorArrows[i].split('').map(char => notationMap[char] || char).join('');
+    }
+  }
 
 }
 
