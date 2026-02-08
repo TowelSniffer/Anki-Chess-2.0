b3b83f0a73f1c36a71fa6d7961d82e8a771edbaa
TowelSniffer <fionnbharr99@gmail.com>
2026-02-09 04:19:14 +1000

    Ts fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 declare global {
   interface Window {
     mountChess?: () => void;
-    USER_CONFIG?: Record<string, boolean>;
+    USER_CONFIG?: Record<string, boolean | number>;
     CARD_CONFIG?: {
       modelName: string;
       cardName: string;
