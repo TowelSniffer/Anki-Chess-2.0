6a5d91af60d056f61d74440d3b58414bd63c5838
TowelSniffer <fionnbharr99@gmail.com>
2026-02-09 05:27:22 +1000

    Added interface for USER_CONFIG

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,29 @@
+interface UserConfig {
+  flipBoard: boolean;
+  mirror: boolean;
+  showDests: boolean;
+  disableArrows: boolean;
+  singleClickMove: boolean;
+  animationTime: number;
+  handicap: number;
+  autoAdvance: boolean;
+  handicapAdvance: boolean;
+  timerAdvance: boolean;
+  strictScoring: boolean;
+  acceptVariations: boolean;
+  timer: number;
+  increment: number;
+  randomOrientation: boolean;
+  analysisTime: number;
+  analysisLines: number;
+  muteAudio: boolean;
+  frontText: boolean;
+}
+
 declare global {
   interface Window {
     mountChess?: () => void;
-    USER_CONFIG?: Record<string, boolean | number>;
+    USER_CONFIG?: Partial<UserConfig>;
     CARD_CONFIG?: {
       modelName: string;
       cardName: string;
